94bb5d34f898c084c94387c35a57df59ed39500b
Frank Lucassen <mcfranzl26@t-online.de>
2023-09-18 23:05:25 +0200

    day18 reduce done, need sum up for score

--- src/Day18/Day18.java
+++ src/Day18/Day18.java
@@ -10,24 +10,30 @@ public class Day18 {
     private static final String FILE = "./Input/Test";
 
     public static void main(String[] args) {
-        Node n = parseLine("[[[[[4,3],4],4],[7,[[8,4],9]]],[1,1]]");
-//        Node n = parseLine("[[[[0,7],4],[7,[[8,4],9]]],[1,1]]");
-        explode(n);
+//        Node n = parseLine("[[[[[4,3],4],4],[7,[[8,4],9]]],[1,1]]");
+//        Node n = parseLine("[[[[0,7],4],[15,[0,13]]],[1,1]]");
+//        split(n).print();
 
-//        LinkedList<Node> lines = new LinkedList<>();
-//
-//        try (BufferedReader in = new BufferedReader(new FileReader(FILE))) {
-//            String line = in.readLine();
-//            while (line != null) {
-//                lines.add(parseLine(line));
-//                line = in.readLine();
-//            }
-//        } catch (IOException e) {
-//            throw new RuntimeException(e);
-//        }
-//
-//        Node sum = linesToSum(lines);
-//        sum.print();
+//        Node n = parseLine("[[[[[4,3],4],4],[7,[[8,4],9]]],[1,1]");
+//        reduce(n).print();
+
+        Node n = parseLine("[[[[4,0],[5,4]],[[7,0],[15,5]]],[7,[[[3,7],[4,3]],[[6,3],[8,8]]]]]");
+        explode(n).print();
+        System.out.println("\n\n\n");
+        LinkedList<Node> lines = new LinkedList<>();
+
+        try (BufferedReader in = new BufferedReader(new FileReader(FILE))) {
+            String line = in.readLine();
+            while (line != null) {
+                lines.add(parseLine(line));
+                line = in.readLine();
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+        Node sum = linesToSum(lines);
+        sum.print();
     }
 
     public static Node findFirst5th(Node node, int level) {
@@ -38,26 +44,59 @@ public class Day18 {
         return n;
     }
 
-    public static boolean explode(Node root) {
+    public static Node explode(Node root) {
         Node n = findFirst5th(root, 0);
-        if (n == null) return false;
+        if (n == null) return null;
 
         int toLeft = n.left.value;
         int toRight = n.right.value;
         Node neu = new Node(n.parent, 0);
         if (Objects.equals(n.parent.left, n)) n.parent.setLeft(neu);
         else n.parent.setRight(neu);
+        int printAt = wouldPrintAt(root, neu);
 
         done = false;
-        System.out.println(wouldPrintAt(root, neu));
-        root.print();
+        StringBuilder build = new StringBuilder();
+        buildString(root, build);
+        String s = build.toString();
         // left
-//        for (int i = printAt; i > 0; i++) {
-//
-//        }
-
-
-        return true;
+        for (int i = printAt - 1; i > 0; i--) {
+            char c = s.charAt(i);
+            if ('0' <= c && c <= '9') {
+                char c2 = s.charAt(i - 1);
+                int val;
+                if ('0' <= c2 && c2 <= '9') {
+                    val = Integer.parseInt("" + c2 + c);
+                    val += toLeft;
+                    s = s.substring(0, i - 1) + val + s.substring(i + 1);
+                } else {
+                    val = Integer.parseInt("" + c);
+                    val += toLeft;
+                    s = s.substring(0, i) + val + s.substring(i + 1);
+                }
+                if (val > 9) printAt++;
+                break;
+            }
+        }
+        // right
+        for (int i = printAt + 2; i < s.length(); i++) {
+            char c = s.charAt(i);
+            if ('0' <= c && c <= '9') {
+                char c2 = s.charAt(i + 1);
+                int val;
+                if ('0' <= c2 && c2 <= '9') {
+                    val = Integer.parseInt("" + c + c2);
+                    val += toRight;
+                    s = s.substring(0, i) + val + s.substring(i + 2);
+                } else {
+                    val = Integer.parseInt("" + c);
+                    val += toRight;
+                    s = s.substring(0, i) + val + s.substring(i + 1);
+                }
+                break;
+            }
+        }
+        return parseLine(s);
     }
 
     static boolean done = false;
@@ -68,7 +107,8 @@ public class Day18 {
             return 0;
         }
         if (root.value != null) {
-            return 1;
+            if (root.value < 10) return 1;
+            else return 2;
         }
         int i = 1;
         i += wouldPrintAt(root.left, node);
@@ -79,13 +119,62 @@ public class Day18 {
         return i + 1;
     }
 
-    public static boolean split(Node root) {
-        return false;
+    public static void buildString(Node root, StringBuilder build) {
+        if (root.value != null) {
+            build.append(root.value);
+            return;
+        }
+        build.append('[');
+        buildString(root.left, build);
+        build.append(',');
+        buildString(root.right, build);
+        build.append(']');
     }
 
-    public static void reduce(Node root) {
+    public static Node findFirstSplit(Node node) {
+        if (node.value != null) {
+            if (node.value > 9) return node;
+            return null;
+        }
+        Node n = null;
+        n = findFirstSplit(node.left);
+        if (n == null) n = findFirstSplit(node.right);
+        return n;
+    }
+
+    public static Node split(Node root) {
+        Node n = findFirstSplit(root);
+        if (n == null) return null;
+
+        int val = n.value;
+        int left = Math.floorDiv(val, 2);
+        int right = Math.ceilDiv(val, 2);
+
+        Node neu = new Node(n.parent);
+        neu.setLeft(new Node(neu, left));
+        neu.setRight(new Node(neu, right));
+
+        if (Objects.equals(n.parent.left, n)) {
+            n.parent.setLeft(neu);
+        } else {
+            n.parent.setRight(neu);
+        }
+
+
+        return root;
+    }
+
+    public static Node reduce(Node root) {
+        Node tmp = root;
+        Node ret;
         do {
-        } while (explode(root) || split(root));
+            System.out.print("red: ");
+            tmp.print();
+            ret = explode(tmp);
+            if (ret == null) ret = split(tmp);
+            if (ret != null) tmp = ret;
+        } while (ret != null);
+        return tmp;
     }
 
     public static Node linesToSum(LinkedList<Node> lines) {
@@ -93,13 +182,16 @@ public class Day18 {
         Node root = new Node(null);
         root.setLeft(arr[0]);
         root.setRight(arr[1]);
-        reduce(root);
+        root.print();
+        root = reduce(root);
         for (int i = 2; i < arr.length; i++) {
+            System.out.print("sum: ");
+            root.print();
             Node tmp = new Node(null);
             tmp.setLeft(root);
             tmp.setRight(arr[i]);
             root = tmp;
-            reduce(root);
+            root = reduce(root);
         }
         return root;
     }
@@ -121,7 +213,14 @@ public class Day18 {
             } else if (c == ']') {
                 cur = cur.parent;
             } else if ('0' <= c && c <= '9') {
-                int num = Integer.parseInt("" + c);
+                char c2 = line.charAt(i + 1);
+                int num;
+                if ('0' <= c2 && c2 <= '9') {
+                    i++;
+                    num = Integer.parseInt("" + c + c2);
+                } else {
+                    num = Integer.parseInt("" + c);
+                }
                 if (right) cur.right = new Node(cur, num);
                 else cur.left = new Node(cur, num);
             } else {
